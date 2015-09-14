97d07fb2fd6c31afa6d5efb7a27cc40a25b96e55
user01 <user01@mapr1node.(none)>
2015-09-14 02:14:48 -0700

    Built jar file and modified bash scripts for my cluster.  All works and runs.

--- Labs/Lab1/rebuild.sh
+++ Labs/Lab1/rebuild.sh
@@ -5,8 +5,10 @@ export LD_LIBRARY_PATH=$HADOOP_HOME/lib/native/Linux-amd64-64
 export CLASSPATH=$HADOOP_HOME/*:$HADOOP_HOME/lib/* 
 export HADOOP_CLASSPATH=$CLASSPATH
 
-javac -d classes IrisMapper.java
-javac -d classes IrisReducer.java
-jar -cvf Iris.jar -C classes/ .
-javac -classpath $CLASSPATH:Iris.jar -d classes IrisDriver.java
-jar -uvf Iris.jar -C classes/ .
+mkdir -p bin
+rm -rf Iris.jar
+javac -d bin ./src/Iris/IrisMapper.java
+javac -d bin ./src/Iris/IrisReducer.java
+jar -cvf Iris.jar -C bin/ .
+javac -classpath $CLASSPATH:Iris.jar -d bin ./src/Iris/IrisDriver.java
+jar -uvf Iris.jar -C bin/ .

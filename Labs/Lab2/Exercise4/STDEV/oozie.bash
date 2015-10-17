f88023a15fb2f9b95571a969e07c51bbcd724ac2
Zayd Hammoudeh <zayd.hammoudeh@sjsu.edu>
2015-10-17 16:20:08 -0700

    Fix to port number in oozie job launcher.

--- Labs/Lab2/Exercise4/STDEV/oozie.bash
+++ Labs/Lab2/Exercise4/STDEV/oozie.bash
@@ -1,8 +1,9 @@
 export OOZIE_URL='http://mapr1node:11000/oozie'
+JobTracker=mapr1node:8032
 
 # Put the data in the HDFS location called by the oozie flow
 hadoop fs -rmr /user/user01/lab2/exercise4/DATA
-hadoop fs -mkdir /user/user01/lab2/exercise4/DATA
+hadoop fs -mkdir -p /user/user01/lab2/exercise4/DATA
 hadoop fs -put DATA/iris-data.txt /user/user01/lab2/exercise4/DATA
 
 # Delete any existing directory for lab 2 exercise 4
@@ -11,7 +12,7 @@ hadoop fs -rmr /user/user01/lab2/exercise4/zayd_hammoudeh_lab2_exercise4
 hadoop fs -put zayd_hammoudeh_lab2_exercise4 /user/user01/lab2/exercise4/zayd_hammoudeh_lab2_exercise4
 
 # Run the oozie job
-/opt/mapr/oozie/oozie-4.1.0/bin/oozie job -oozie="http://marp1node:111000/oozie" -config /user/user01/lab2/exercise4/zayd_hammoudeh_lab2_exercise4/job.properties -run
+/opt/mapr/oozie/oozie-4.1.0/bin/oozie job -oozie="http://marp1node:11000/oozie" -config /user/user01/lab2/exercise4/zayd_hammoudeh_lab2_exercise4/job.properties -run
 
 
 # Check job status. Replace <JobName> with the actual job name

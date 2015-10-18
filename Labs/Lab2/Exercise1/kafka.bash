e7325f55daec3cfe4a9cb7ec1adbf321384e3244
Zayd Hammoudeh <zayd.hammoudeh@sjsu.edu>
2015-10-18 03:34:34 -0700

    Fixing producer port

--- Labs/Lab2/Exercise1/kafka.bash
+++ Labs/Lab2/Exercise1/kafka.bash
@@ -11,7 +11,7 @@ $KAFKA_DIR/bin/kafka-topics.sh --zookeeper localhost:5181 --create --topic logte
 $KAFKA_DIR/bin/kafka-topics.sh --zookeeper localhost:5181 --list
 
 # Create a Kafka Producer
-$KAFKA_DIR/bin/kafka-console-producer.sh --broker-list localhost:5140 --topic logtest 
+$KAFKA_DIR/bin/kafka-console-producer.sh --broker-list localhost:9092 --topic logtest 
 
 # Create a Kafka Consumer
 $KAFKA_DIR/bin/kafka-console-consumer.sh --zookeeper localhost:5181 --topic test --from-beginning

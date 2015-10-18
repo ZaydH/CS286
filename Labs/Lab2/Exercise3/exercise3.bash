5d8df5ad301ff37518ffb2387e5de9a2fa34a47b
Zayd Hammoudeh <zayd.hammoudeh@sjsu.edu>
2015-10-18 00:53:22 -0700

    More work on the debug for the exercise3 with Sqoop

--- Labs/Lab2/Exercise3/exercise3.bash
+++ Labs/Lab2/Exercise3/exercise3.bash
@@ -6,3 +6,7 @@ hadoop fs -mkdir -p /user/user01/lab2/exercise3/DATA
 cp iris-data.txt /user/user01/lab2/exercise3/DATA
 
 hive -f exercise3.hql
+
+mysql > DROP DATABASE IF EXISTS zayd_hammoudeh;
+mysql > CREATE DATABASE zayd_hammoudeh;
+

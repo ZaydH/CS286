878f3763dcceff9ae0450abd9b5f81fa7b93f781
Zayd Hammoudeh <zayd.hammoudeh@sjsu.edu>
2015-10-19 00:17:15 -0700

    Successfully changed the agent name from a1 to agent.

--- Labs/Lab2/Exercise2/flume.bash
+++ Labs/Lab2/Exercise2/flume.bash
@@ -1,1 +1,1 @@
-/opt/mapr/flume/flume-1.6.0/bin/flume-ng agent --conf conf --conf-file syslog-flume.conf --name a1 -Dflume.root.logger=INFO,console
+/opt/mapr/flume/flume-1.6.0/bin/flume-ng agent --conf conf --conf-file syslog-flume.conf --name agent -Dflume.root.logger=INFO,console

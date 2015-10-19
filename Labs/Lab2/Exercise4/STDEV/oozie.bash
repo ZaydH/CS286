976d69afe27050301be9392eae791bf08571ac0e
Zayd Hammoudeh <zayd.hammoudeh@sjsu.edu>
2015-10-18 19:39:45 -0700

    Some progress debugging the oozie workflow.  At least it is able to partially run using oozie.bash

--- Labs/Lab2/Exercise4/STDEV/oozie.bash
+++ Labs/Lab2/Exercise4/STDEV/oozie.bash
@@ -1,5 +1,9 @@
-export OOZIE_URL='http://mapr1node:11000/oozie'
+#export OOZIE_URL='http://mapr1node:11000/oozie'
 
+# Delete any existing directory for lab 2 exercise 4
+hadoop fs -rmr /user/user01/lab2/exercise4/zayd_hammoudeh_lab2_exercise4
+# Put a new copy of lab 2 exercise 4 files
 hadoop fs -put zayd_hammoudeh_lab2_exercise4 /user/user01/lab2/exercise4/zayd_hammoudeh_lab2_exercise4
 
-oozie job -oozie exercise4-oozie.xml 
+# Run the oozie job
+/opt/mapr/oozie/oozie-4.1.0/bin/oozie job -oozie="http://localhost:11000/oozie" -config /user/user01/lab2/exercise4/zayd_hammoudeh_lab2_exercise4/job.properties -run

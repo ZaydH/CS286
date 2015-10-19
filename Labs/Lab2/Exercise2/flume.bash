9f94ab3b6929aefe9e28e7728dd68fe7e2aa75fc
Zayd Hammoudeh <zayd.hammoudeh@sjsu.edu>
2015-10-18 23:57:03 -0700

    Renaming the exercise 2 files to match the lab 2 modified version.

--- Labs/Lab2/Exercise2/flume.bash
+++ Labs/Lab2/Exercise2/flume.bash
@@ -1,1 +1,1 @@
-/opt/mapr/flume/flume-1.6.0/bin/flume-ng agent --conf conf --conf-file flume.conf --name a1 -Dflume.root.logger=INFO,console
+/opt/mapr/flume/flume-1.6.0/bin/flume-ng agent --conf conf --conf-file syslog-flume.conf --name a1 -Dflume.root.logger=INFO,console

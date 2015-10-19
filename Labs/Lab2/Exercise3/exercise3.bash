6ebd584f66b6c9b7c7b0d808f1ca3f2c608317d3
Zayd Hammoudeh <zayd.hammoudeh@sjsu.edu>
2015-10-19 02:32:54 -0700

    Adding placeholder file for the sqoop tool for iris.

--- Labs/Lab2/Exercise3/exercise3.bash
+++ Labs/Lab2/Exercise3/exercise3.bash
@@ -7,6 +7,8 @@ cp iris-data.txt /user/user01/lab2/exercise3/DATA
 
 hive -f exercise3.hql
 
-mysql > DROP DATABASE IF EXISTS zayd_hammoudeh;
-mysql > CREATE DATABASE zayd_hammoudeh;
+#su root
+#mysql > USE default;
+#mysql > CREATE TABLE iris_table (id INT NOT NULL AUTO_INCREMENT, sepalLength FLOAT NOT NULL, sepalWidth FLOAT NOT NULL, petalLength FLOAT NOT NULL, petalWidth FLOAT NOT NULL, PRIMARY KEY (id) );
+
 
---
+++ Labs/Lab2/Exercise3/iris-sqoop-2-mysql.sh
@@ -0,0 +1,1 @@
+qoop export --connect jdbc:mysql://localhost/default --username root --table iris-table --export-dir /user/hive/warehouse/iris-table/

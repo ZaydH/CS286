05af7ca27efa8315d219293aac94298a8edbb7a8
Zayd Hammoudeh <zayd.hammoudeh@sjsu.edu>
2015-10-21 04:21:18 -0700

    Final start pipeline for exercise #1

--- Labs/Lab2/Exercise1/start-pipeline.sh
+++ Labs/Lab2/Exercise1/start-pipeline.sh
@@ -1,2 +1,2 @@
-/opt/mapr/flume/flume-1.6.0/bin/flume-ng agent --conf conf --conf-file kafka-flume.conf --name agent -Dflume.root.logger=INFO,console
+/opt/mapr/flume/flume-1.6.0/bin/flume-ng agent --conf conf --conf-file kafka-flume.conf --name tier1 -Dflume.root.logger=INFO,console
 

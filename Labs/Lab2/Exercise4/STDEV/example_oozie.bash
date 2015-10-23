ddef910c8573e4e1f0fde8b1d63062358470ca33
user01 <user01@mapr1node.(none)>
2015-10-23 04:53:54 -0700

    Adding code for the modified example trying to run oozie.

--- Labs/Lab2/Exercise4/STDEV/example_oozie.bash
+++ Labs/Lab2/Exercise4/STDEV/example_oozie.bash
@@ -1,13 +1,13 @@
-export OOZIE_URL='http://mapr1node:11000/oozie'
+export OOZIE_URL="http://localhost:11000/oozie"
 export OOZIE_HOME=/opt/mapr/oozie/oozie-4.1.0
 export PATH=$PATH:$OOZIE_HOME/bin
 
 # Put the data in the HDFS location called by the oozie flow
 hadoop fs -rmr /user/user01/map-reduce
 hadoop fs -mkdir -p /user/user01/map-reduce/DATA
-hadoop fs -put /home/user01/CS286/Labs/Lab2/Exercise4/STDEV/DATA/iris-data.txt /user/user01/map-reduce/DATA
+hadoop fs -put $PWD/DATA/iris-data.txt /user/user01/map-reduce/DATA
 mkdir -p /user/user01/map-reduce/DATA
-cp /home/user01/CS286/Labs/Lab2/Exercise4/STDEV/DATA/iris-data.txt /user/user01/map-reduce/DATA/iris-data22.txt
+cp DATA/iris-data.txt /user/user01/map-reduce/DATA/iris-data22.txt
 
 # Put a new copy of lab 2 exercise 4 files
 hadoop fs -put map-reduce /user/user01/ 

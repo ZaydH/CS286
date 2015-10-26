0f46fad27135afdbc5284fcb8c051eb0a1b89181
Zayd Hammoudeh <zayd.hammoudeh@sjsu.edu>
2015-10-25 21:27:22 -0700

    More work on the rerun.sh script for exercise #1

--- Labs/Lab2/Exercise1/rerun.sh
+++ Labs/Lab2/Exercise1/rerun.sh
@@ -7,4 +7,8 @@ $KAFKA_DIR/bin/kafka-topics.sh --zookeeper localhost:5181 --create --topic zayd_
 # Verify the topic was created
 $KAFKA_DIR/bin/kafka-topics.sh --zookeeper localhost:5181 --list
 
+# Start the flume producer
+/opt/mapr/flume/flume-1.6.0/bin/flume-ng agent --conf conf --conf-file kafka-flume.conf --name tier1 -Dflume.root.logger=INFO,console
+sleep 5 # Wait for the flume agent to start
+
 /opt/mapr/spark/spark-1.4.1/bin/spark-submit --class org.apache.spark.examples.streaming.JavaKafkaWordCount --master local[2] spark-zayd/examples/target/spark-examples_2.10-1.6.0-SNAPSHOT-jar-with-dependencies.jar localhost:5181 default zayd_hammoudeh 1

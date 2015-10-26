1ea806b60b5c8848e0aba9bcbb6cbf411268db4f
Zayd Hammoudeh <zayd.hammoudeh@sjsu.edu>
2015-10-25 22:54:15 -0700

    More compiling of the submission script for lab 2.

--- Labs/Lab2/compile_submission.bash
+++ Labs/Lab2/compile_submission.bash
@@ -6,7 +6,7 @@ cp Exercise1/kafka-flume.conf $SUBMISSION_DIR/kafka-flume.conf
 cp Exercise1/rebuild.sh $SUBMISSION_DIR/rebuild.sh
 cp Exercise1/rerun.sh $SUBMISSION_DIR/rerun.sh
 rm -rf Exercise1/spark-zayd/target
-cp -r Exercise1/spark-zayd zayd_hammoudeh
+cp -r Exercise1/spark-zayd $SUBMISSION_DIR
 
 
 # Copy the Files for Exercise 2
@@ -16,7 +16,12 @@ cp Exercise2/rsyslog.conf $SUBMISSION_DIR/rsyslog.conf
 # Copy the files for Exercise 3
 cp Exercise3/hive-iris.ql $SUBMISSION_DIR/hive-iris.ql
 cp Exercise3/iris-sqoop-2-mysql.sh $SUBMISSION_DIR/iris-sqoop-2-mysql.sh
+cp Exercise3/iris-data.txt /user/user01
 
+# Copy the files for Exercise #4
+#cp Exercise4/STDEV/rebuild.sh $SUBMISSION_DIR
+cp Exercise4/STDEV/rerun.sh $SUBMISSION_DIR/oozie_test.sh
+cp -r Exercise4/STDEV/zayd_hammoudeh_lab2_exercise4 $SUBMISSION_DIR
 
 
 cp -r zayd_hammoudeh /user/user01/zayd_hammoudeh

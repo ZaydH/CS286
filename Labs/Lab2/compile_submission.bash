2a9452f6635a2909e6e3995cd3c27c3931dbb778
Zayd Hammoudeh <zayd.hammoudeh@sjsu.edu>
2015-10-25 23:10:56 -0700

    More fixes to the compile submission script.

--- Labs/Lab2/compile_submission.bash
+++ Labs/Lab2/compile_submission.bash
@@ -1,11 +1,14 @@
 export SUBMISSION_DIR="zayd_hammoudeh"
 
+# Clean any previous versions of the submission directory.
+rm -rf $SUBMISSION_DIR
+
 # Copy the Files for Exercise 1
 cp Exercise1/start-pipeline.sh $SUBMISSION_DIR/start-pipeline.sh
 cp Exercise1/kafka-flume.conf $SUBMISSION_DIR/kafka-flume.conf
 cp Exercise1/rebuild.sh $SUBMISSION_DIR/rebuild.sh
 cp Exercise1/rerun.sh $SUBMISSION_DIR/rerun.sh
-rm -rf Exercise1/spark-zayd/target
+-rm -rf Exercise1/spark-zayd/target
 cp -r Exercise1/spark-zayd $SUBMISSION_DIR
 
 
@@ -17,6 +20,7 @@ cp Exercise2/rsyslog.conf $SUBMISSION_DIR/rsyslog.conf
 cp Exercise3/hive-iris.ql $SUBMISSION_DIR/hive-iris.ql
 cp Exercise3/iris-sqoop-2-mysql.sh $SUBMISSION_DIR/iris-sqoop-2-mysql.sh
 cp Exercise3/iris-data.txt /user/user01
+cp Exercise3/exercise3.bash $SUBMISSION_DIR
 
 # Copy the files for Exercise #4
 #cp Exercise4/STDEV/rebuild.sh $SUBMISSION_DIR

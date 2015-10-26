dab441a7783cdbf22be5ef2be7af74544d7cec13
Zayd Hammoudeh <zayd.hammoudeh@sjsu.edu>
2015-10-25 21:23:33 -0700

    Working on the exercise #1 rerun.sh to start all kafka stuff from a single bash script

--- Labs/Lab2/Exercise1/rerun.sh
+++ Labs/Lab2/Exercise1/rerun.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-/opt/mapr/spark/spark-1.4.1/bin/spark-submit --class org.apache.spark.examples.streaming.JavaKafkaWordCount --master local[2] spark-zayd/examples/target/spark-examples_2.10-1.6.0-SNAPSHOT-jar-with-dependencies.jar localhost:5181 default logtest 1
+
+# Create a zayd_hammoudeh topic
+$KAFKA_DIR/bin/kafka-topics.sh --zookeeper localhost:5181 --create --topic zayd_hammoudeh --partitions 1 --replication-factor 1
+
+# Verify the topic was created
+$KAFKA_DIR/bin/kafka-topics.sh --zookeeper localhost:5181 --list
+
+/opt/mapr/spark/spark-1.4.1/bin/spark-submit --class org.apache.spark.examples.streaming.JavaKafkaWordCount --master local[2] spark-zayd/examples/target/spark-examples_2.10-1.6.0-SNAPSHOT-jar-with-dependencies.jar localhost:5181 default zayd_hammoudeh 1

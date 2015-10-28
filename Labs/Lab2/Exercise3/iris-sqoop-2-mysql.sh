1660effa88c174c179c9796034fa6552dacec558
Zayd Hammoudeh <zayd.hammoudeh@sjsu.edu>
2015-10-28 04:48:43 -0700

    Debugged the code to create the IRIS_TABLE in MySQL in the iris-sqoop-2-mysql.sh script.

--- Labs/Lab2/Exercise3/iris-sqoop-2-mysql.sh
+++ Labs/Lab2/Exercise3/iris-sqoop-2-mysql.sh
@@ -1,1 +1,24 @@
+#!/bin/bash
+
+# Create the MySQL Table
+echo "Create iris_table in MySQL"
+mysql -u user01 -pmapr << EOF
+USE default;
+DROP TABLE IF EXISTS iris_table;
+CREATE TABLE iris_table (sepalLength FLOAT, sepalWidth FLOAT, petalLength FLOAT, petalWidth FLOAT, flowerID FLOAT );
+SELECT * FROM iris_table;
+EOF
+echo "Table successfully created."
+
+# Transfer the data from Hive to MySQL via Sqoop
+echo "Copying the data from Hive to MySQL via Scoop"
 sqoop export --connect jdbc:mysql://localhost/default --username root --table iris_table --export-dir /user/hive/warehouse/iris_table/ --fields-terminated-by '\t'
+echo "Data transfer successful."
+
+
+# Verify the table was created and the data was transferred
+mysql -u user01 -pmapr << EOF
+USE default;
+SELECT * FROM iris_table;
+SELECT COUNT(petalLength) FROM iris_table;
+EOF

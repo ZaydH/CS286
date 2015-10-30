0636a2bc8c5568781a365d23a25c2c6967f2090c
Zayd Hammoudeh <zayd.hammoudeh@sjsu.edu>
2015-10-30 07:22:31 -0700

    I believe submission is now ready. Doing final tests.

---
+++ Labs/Lab2/submission/E4/oozie.bash
@@ -0,0 +1,15 @@
+export OOZIE_URL='http://mapr1node:11000/oozie'
+export OOZIE_HOME=/opt/mapr/oozie/oozie-4.1.0
+export PATH=$PATH:$OOZIE_HOME/bin
+
+mkdir lib
+cp Iris.jar lib
+
+# Run the oozie job
+/opt/mapr/oozie/oozie-4.1.0/bin/oozie job -oozie="http://localhost:11000/oozie" -config /user/user01/LAB2_SUBMISSION/E4/job.properties -run
+
+
+# Check job status. Replace <JobName> with the actual job name
+#/opt/mapr/oozie/oozie-4.1.0/bin/oozie job -oozie http://localhost:11000/oozie -info <JobName>
+#/opt/mapr/oozie/oozie-4.1.0/bin/oozie job -oozie http://localhost:11000/oozie -log <JobName>
+
--- Labs/Lab2/submission/E4/rerun.sh
+++ Labs/Lab2/submission/E4/rerun.sh
@@ -5,4 +5,5 @@ export LD_LIBRARY_PATH=$HADOOP_HOME/lib/native/Linux-amd64-64
 export CLASSPATH=$HADOOP_HOME/lib/*:$HADOOP_HOME/*
 export HADOOP_CLASSPATH=$CLASSPATH
 
+rm -rf /user/user01/oozie_iris_stdev
 hadoop jar Iris.jar Stdev.StdevDriver file:///user/user01/iris-data.txt /user/user01/oozie_iris_stdev 

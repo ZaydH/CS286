7b826c9a527df8b93af88f9ead8981a733948c83
Zayd Hammoudeh <zayd.hammoudeh@sjsu.edu>
2015-11-27 04:38:16 -0800

    Rewriting the script to test KMeans.

--- Labs/Lab3/build_submission.sh
+++ Labs/Lab3/build_submission.sh
@@ -4,7 +4,7 @@ export SUBMISSION_DIR=LAB3_SUBMISSION
 export EXERCISE1_DIR=E1
 export EXERCISE2_DIR=E2
 export EXERCISE3_DIR=E3
-
+export LAB_DIRECTORY=/user/user01
 
 # Delete the submission directory then remake it
 echo "Deleting the existing submission directory."
@@ -21,6 +21,7 @@ cp KMeans_KNN/src/cs286/PointSet.java $SUBMISSION_DIR/$EXERCISE1_DIR/
 cp KMeans_KNN/src/cs286/Kmeans.java $SUBMISSION_DIR/$EXERCISE1_DIR/
 cp KMeans_KNN/manifests/kmeans_manifest.txt $SUBMISSION_DIR/$EXERCISE1_DIR/MANIFEST.MF
 cp kmeans_test.sh $SUBMISSION_DIR/$EXERCISE1_DIR/
+cp iris-data-labeled.txt $SUBMISSION_DIR/$EXERCISE1_DIR/iris-data.txt
 
 # Build Exercise #2
 echo "Copying the files for exercise #2"
@@ -31,9 +32,12 @@ cp KMeans_KNN/src/cs286/PointSet.java $SUBMISSION_DIR/$EXERCISE2_DIR/
 cp KMeans_KNN/src/cs286/Knn.java $SUBMISSION_DIR/$EXERCISE2_DIR/
 cp KMeans_KNN/manifests/knn_manifest.txt $SUBMISSION_DIR/$EXERCISE2_DIR/MANIFEST.MF
 cp knn_test.sh $SUBMISSION_DIR/$EXERCISE2_DIR/
+cp iris-data-labeled.txt $SUBMISSION_DIR/$EXERCISE2_DIR/iris-data.txt
 
-## Build Exercise #3
-#mkdir -p $SUBMISSION_DIR/$EXERCISE3_DIR
+# Copy the files into the
+echo "Creating the lab directory."
+rm -rf $LAB_DIRECTORY/$SUBMISSION_DIR &> /dev/null
+cp -r $SUBMISSION_DIR $LAB_DIRECTORY
 
-
+# End of the Bash creation script
 echo "Submission building completed successfully."
--- Labs/Lab3/kmeans_test.sh
+++ Labs/Lab3/kmeans_test.sh
@@ -4,19 +4,245 @@
 export INPUT_FILE=$1
 export OUT_DIR=test_results
 
+# Rebuild the JAR just to be extra dilligent
+printf "Building the KMeans JAR file.\n"
+bash rebuild.sh &> /dev/null
+printf "KMeans JAR file built.\n\n\n"
+
 # Create a new test results folder
 rm -rf $OUT_DIR &> /dev/null
 mkdir $OUT_DIR
 
+# Initialize the counter
+COUNTER=1
+
 # Do an extra input parameter check
-export k=1
+printf "Test #${COUNTER} - Too many parameter check.\n"
+export k=3
 export maxIterations=1
 export delta=1
 export dist=cosine
+export init=random
 export inputFile=$INPUT_FILE
 export outputFile=dummy
-echo "Perform an extra parameter check."
-java kmeans.jar cs286.Kmeans $k $maxIterations $delta $dist $inputFile $outputFile extra
-if [$? -eq 0]; then
-	echo "ERROR: No invalid parameter count check."
+bash rerun.sh $k $maxIterations $delta $dist $init $inputFile $outputFile extra
+if [ $? -eq 0 ]; then
+	printf "ERROR: Returned value 0."
+else
+	printf "PASSED"
 fi
+printf "\n\n\n"
+let COUNTER=COUNTER+1
+
+# Perform insufficient parameter count check
+printf "Test #${COUNTER} - Insufficient parameter check.\n"
+export k=3
+export maxIterations=1
+export delta=1
+export dist=cosine
+export init=random
+export inputFile=$INPUT_FILE
+export outputFile=dummy
+bash rerun.sh $k $maxIterations $delta $dist $init $inputFile  
+if [ $? -eq 0 ]; then
+        printf "ERROR: Returned value 0."
+else
+        printf "PASSED"
+fi
+printf "\n\n\n"
+let COUNTER=COUNTER+1
+
+
+# Test #03 - Perform invalid value of k test
+export k=0
+export maxIterations=1
+export delta=1
+export dist=cosine
+export init=random
+export inputFile=$INPUT_FILE
+export outputFile=dummy.txt
+
+printf "Test #${COUNTER} - k = ${k} test.\n"
+bash rerun.sh $k $maxIterations $delta $dist $init $inputFile $outputFile
+if [ $? -eq 0 ]; then
+        printf "ERROR: Returned value 0."
+else
+        printf "PASSED"
+fi
+printf "\n\n\n"
+let COUNTER=COUNTER+1
+
+
+
+
+# Test #04 - Perform invalid value of max iterations test
+export k=4
+export maxIterations=0
+export delta=1
+export dist=cosine
+export init=random
+export inputFile=$INPUT_FILE
+export outputFile=dummy.txt
+
+printf "Test #${COUNTER} - max-iterations = ${maxIterations} test.\n"
+bash rerun.sh $k $maxIterations $delta $dist $init $inputFile $outputFile
+if [ $? -eq 0 ]; then
+        printf "ERROR: Returned value 0."
+else
+        printf "PASSED"
+fi
+printf "\n\n\n"
+let COUNTER=COUNTER+1
+
+
+
+
+# Test #05 - Delta = 0 Test
+export k=4
+export maxIterations=50
+export delta=0
+export dist=cosine
+export init=random
+export inputFile=$INPUT_FILE
+export outputFile=dummy.txt
+
+printf "Test #${COUNTER} - delta = ${delta} test.\n"
+bash rerun.sh $k $maxIterations $delta $dist $init $inputFile $outputFile
+if [ $? -eq 0 ]; then
+        printf "ERROR: Returned value 0."
+else
+        printf "PASSED"
+fi
+printf "\n\n\n"
+let COUNTER=COUNTER+1
+
+
+
+
+# Test #06 - Delta = -1 Test
+export k=4
+export maxIterations=50
+export delta=-1
+export dist=cosine
+export init=random
+export inputFile=$INPUT_FILE
+export outputFile=dummy.txt
+
+printf "Test #${COUNTER} - delta = ${delta} test.\n"
+bash rerun.sh $k $maxIterations $delta $dist $init $inputFile $outputFile
+if [ $? -eq 0 ]; then
+        printf "ERROR: Returned value 0."
+else
+        printf "PASSED"
+fi
+printf "\n\n\n"
+let COUNTER=COUNTER+1
+
+
+
+# Test #07 - Invalid distance metric test
+export k=4
+export maxIterations=50
+export delta=3
+export dist=dummy
+export init=random
+export inputFile=$INPUT_FILE
+export outputFile=dummy.txt
+
+printf "Test #${COUNTER} - dist = ${dist} test.\n"
+bash rerun.sh $k $maxIterations $delta $dist $init $inputFile $outputFile
+if [ $? -eq 0 ]; then
+        printf "ERROR: Returned value 0."
+else
+        printf "PASSED"
+fi
+printf "\n\n\n"
+let COUNTER=COUNTER+1
+
+
+
+
+# Test #08 - Invalid centroid initialization test
+export k=4
+export maxIterations=50
+export delta=3
+export dist=cosine
+export init=dummy
+export inputFile=$INPUT_FILE
+export outputFile=dummy.txt
+
+printf "Test #${COUNTER} - init = ${init} test.\n"
+bash rerun.sh $k $maxIterations $delta $dist $init $inputFile $outputFile
+if [ $? -eq 0 ]; then
+        printf "ERROR: Returned value 0."
+else
+        printf "PASSED"
+fi
+printf "\n\n\n"
+let COUNTER=COUNTER+1
+
+
+
+
+# Test #09 - Invalid input file
+export k=4
+export maxIterations=50
+export delta=3
+export dist=cosine
+export init=random
+export inputFile=dummyFile
+export outputFile=dummy.txt
+
+printf "Test #${COUNTER} - inputFile = ${INPUT_FILE} test.\n"
+bash rerun.sh $k $maxIterations $delta $dist $init $inputFile $outputFile
+if [ $? -eq 0 ]; then
+        printf "ERROR: Returned value 0."
+else
+        printf "PASSED"
+fi
+printf "\n\n\n"
+let COUNTER=COUNTER+1
+
+
+
+
+# Test #10 - Invalid output file
+export k=4
+export maxIterations=50
+export delta=3
+export dist=cosine
+export init=random
+export inputFile=$INPUT_FILE
+export outputFile=$INPUT_FILE
+
+printf "Test #${COUNTER} - inputFile = outputFile = ${INPUT_FILE} test.\n"
+bash rerun.sh $k $maxIterations $delta $dist $init $inputFile $outputFile
+if [ $? -eq 0 ]; then
+        printf "ERROR: Returned value 0."
+else
+        printf "PASSED"
+fi
+printf "\n\n\n"
+let COUNTER=COUNTER+1
+
+
+
+
+# Test - Invalid centroid initialization test
+export k=4
+export maxIterations=50
+export delta=600
+export dist=cosine
+export init=random
+export inputFile=$INPUT_FILE
+export outputFile="$OUT_DIR/${k}_${maxIterations}_${delta}_${dist}_${init}.txt"
+
+printf "Test #${COUNTER} - maxitr =\"${maxIterations}\", delta=\"${delta}\", dist=\"${dist}\", init=\"${init}\" test.\n"
+bash rerun.sh $k $maxIterations $delta $dist $init $inputFile $outputFile
+if [ $? -ne 0 ]; then
+        printf "ERROR: Did not return value 0."
+else
+        printf "PASSED"
+fi
+printf "\n\n\n"
+let COUNTER=COUNTER+1

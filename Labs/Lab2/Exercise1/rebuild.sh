604393b183a4764d6e705c27b3e633083583b8ae
Zayd Hammoudeh <zayd.hammoudeh@sjsu.edu>
2015-12-18 04:17:15 -0800

    Changing the rebuild script to avoid the compile error.  Did not used to happen.  Seems it is caused by a change in libraries affecting one of the test files I had that I did not really need.

--- Labs/Lab2/Exercise1/rebuild.sh
+++ Labs/Lab2/Exercise1/rebuild.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+rm spark-zayd/examples/src/main/scala/org/apache/spark/examples/mllib/StreamingTestExample.scala
+
 cd spark-zayd/examples
 mvn assembly:assembly -DdescriptorId=jar-with-dependencies -Denforcer.skip=true
 cd ../..
